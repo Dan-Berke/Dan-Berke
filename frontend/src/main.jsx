fc60c8d63f7eaa805bc409dddf4aa23cc0ef6e01
Andrew S <143370040+codeasch@users.noreply.github.com>
2025-05-08 19:05:05 +0000

    chore: set router basename for Pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// main.jsx
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename="/Dan-Berke/">
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
